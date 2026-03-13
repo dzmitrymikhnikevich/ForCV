76da9e440a47e10734087d5b0fbe38872384d15f
dzmitrymikhnikevich <dzmitrymikhnikevich@gmail.com>
2026-03-14 00:02:25 +0300

    Добавил import By в login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 import allure
+from selenium.webdriver.common.by import By
 from base.base_page import BasePage
 from config.links import Links
 from selenium.webdriver.support import expected_conditions as EC
